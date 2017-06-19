7c0ad444d412bf3a9ac0028666b60a36e6054102
caseysiebel <leptone55@gmail.com>
2017-06-19 15:58:01 -0700

    add fallback

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
 const logger = require('morgan')
+const fallback = require('express-history-api-fallback')
 
 app.use(logger('dev'))
 
 app.get('/square', (req, res) => res.send('square'))
 app.get('/circle', (req, res) => res.send('circle'))
 
-app.use(express.static(__dirname + '/public'))
+const root = __dirname + '/public'
+
+app.use(express.static(root))
+app.use(fallback('/index.html', { root: root }))
 
 app.listen(3000, () => console.log('running'))
